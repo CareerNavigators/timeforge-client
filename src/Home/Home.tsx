47e1c0b971874d8878f851d5b8cb3e4c4ce4b5be
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2024-01-17 23:32:42 +0600

    new text added

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-3xl text-center">Home/TimeForge</h1>
+      <h1>Shabbir</h1>
     </div>
   );
 };
