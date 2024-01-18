3e021c3b617480d6d1cc7712a8d4237484e35306
sakib071 <c193055@ugrad.iiuc.ac.bd>
2024-01-18 21:51:57 +0600

    new update

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,6 +2,7 @@ import Card from "./Card";
 import Hero from "./Hero";
 import Schedule from "../Components/Schedule/Schedule";
 
+
 const Home = () => {
   return (
       <div className="max-w-[1400px] mx-auto ">
