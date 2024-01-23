ce05c0f8a080dab334c6cebf4108aa58c92c3310
tanzilrayhan <tanzilrayhan169@gmail.com>
2024-01-23 21:03:29 +0600

    Calender design added

--- src/Components/EventSlot/EventSlot.tsx
+++ src/Components/EventSlot/EventSlot.tsx
@@ -1,11 +1,19 @@
-
+import CalenderDesign from "./CalenderDesign";
+import Logo from "/logo.png";
 
 const EventSlot = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div className="max-w-5xl shadow-md rounded-lg my-20 mx-auto p-20">
+        <div className="flex justify-between">
+          
+          <section className="shadow-md rounded-lg">
+            <CalenderDesign />
+          </section>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default EventSlot;
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,14 +1,15 @@
 import Card from "./Card";
 import Hero from "./Hero";
 import Schedule from "../Components/Schedule/Schedule";
+import EventSlot from "../Components/EventSlot/EventSlot";
 
 const Home = () => {
   return (
     <div className="max-w-[1400px] mx-auto ">
       <Hero></Hero>
       <Card></Card>
-      <h1 className="text-3xl text-center">Home/TimeForge</h1>
       <Schedule></Schedule>
+      <EventSlot></EventSlot>
     </div>
   );
 };
