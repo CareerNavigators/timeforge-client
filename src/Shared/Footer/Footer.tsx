4827e516ec33965c3225870e319c2ae3387a2cc3
Ruhannn <kamiayaka09@gmail.com>
2024-01-26 00:40:06 +0600

    dark mode done

--- src/Shared/Footer/Footer.tsx
+++ src/Shared/Footer/Footer.tsx
@@ -18,25 +18,25 @@ const Footer = () => {
   );
   return (
     <footer className='bg-white'>
-      <hr className='mx-auto max-w-screen-xl text-gray-600'></hr>
-      <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8'>
+      <hr className='max-w-screen-xl mx-auto text-gray-600'></hr>
+      <div className='max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8'>
         <div className='sm:flex sm:justify-between'>
           <div>
-            <div className='flex items-center justify-center sm:justify-start gap-2'>
+            <div className='flex items-center justify-center gap-2 sm:justify-start'>
               <FaGlobe></FaGlobe>
-              <h3 className='font-semibold text-base'>English</h3>
+              <h3 className='text-base font-semibold'>English</h3>
               <FaChevronDown></FaChevronDown>
             </div>
           </div>
-          <ul className='flex flex-wrap-reverse md:flex-row items-center justify-center gap-3'>
+          <ul className='flex flex-wrap-reverse items-center justify-center gap-3 md:flex-row'>
             {links}
           </ul>
-          <div className='flex items-center justify-center gap-3 mt-4 text-center text-2xl lg:mt-0 lg:text-right'>
+          <div className='flex items-center justify-center gap-3 mt-4 text-2xl text-center lg:mt-0 lg:text-right'>
             <FaFacebook></FaFacebook>
             <FaInstagram></FaInstagram>
           </div>
         </div>
-        <h2 className='text-gray-500 text-center md:text-start text-sm mt-2'>
+        <h2 className='mt-2 text-sm text-center text-gray-500 md:text-start'>
           Copyright TimeForge 2024
         </h2>
       </div>
