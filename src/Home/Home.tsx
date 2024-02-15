c1295812030720863246a8d44f7028734457d419
tanzilrayhan <tanzilrayhan169@gmail.com>
2024-02-15 20:03:49 +0600

    Demo Event Creation

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import Schedule from "../Components/Schedule/Schedule";
 import MarqueeElement from "../Components/Marquee/MarqueeElement";
 import HighlightedEvents from "../Components/HighlightedEvents/HighlightedEvents";
+import DemoEvent from "../Components/Demo/DemoEvent";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       </div>
 
       <Card></Card>
+      <DemoEvent></DemoEvent>
       <Schedule></Schedule>
       <HighlightedEvents></HighlightedEvents>
       <MarqueeElement></MarqueeElement>
