6f78e6f41e6083329020d55ee18eaf4d816d710a
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-02-24 07:03:42 +0600

    timeline reset added

--- src/Dashboard/Admin/Timeline/MyTimline.tsx
+++ src/Dashboard/Admin/Timeline/MyTimline.tsx
@@ -10,25 +10,36 @@ import { Column, PagingOptions } from "ka-table/models";
 import AxiosSecure from "../../../Hook/useAxios";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import moment from "moment";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { SingleTimeLine, TimelineType, TimelineItem } from "../AllTypes";
 import { FormatFunc } from "ka-table/types";
-import {
-  Button,
-  Input,
-  Modal,
-  Spin,
-  TimePicker,
-  Timeline,
-  Tooltip,
-} from "antd";
+import { Button, Input, Modal, Spin, TimePicker, Timeline } from "antd";
+import showToast from "../../../Hook/swalToast";
 const MyTimline = () => {
   const caxios = AxiosSecure();
   const [page, setPage] = useState<number>(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
   const [twoTime, setTwoTime] = useState(null);
   const [content, setContent] = useState("");
-
+  const [selectedSlot, setSelectedSlot] = useState<TimelineItem | undefined>();
+  const handelOpen = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handelOpen2 = (id: string | undefined) => {
+    if (selectedTimeline.data?.timeline) {
+      setSelectedSlot(
+        selectedTimeline.data?.timeline.filter((x) => x._id == id)[0]
+      );
+      setIsModalOpen2(true);
+    }
+  };
+  const handleCancel2 = () => {
+    setIsModalOpen2(false);
+  };
   const selectedTimeline = useMutation({
     mutationFn: async (id: string) => {
       const res = await caxios.get(`/timeline?type=single&id=${id}`);
@@ -95,7 +106,7 @@ const MyTimline = () => {
   };
   const format: FormatFunc = ({ column, rowData }): any => {
     if (column.key == "createdAt") {
-      return moment(rowData.createdAt).format("MMM Do YY, h:mm a").toString();
+      return moment(rowData.createdAt).format("MMM Do YY, h:mm a");
     } else if (column.key == "event") {
       return <span>{rowData.event?.title}</span>;
     } else if (column.key == "action") {
@@ -110,12 +121,6 @@ const MyTimline = () => {
         </Button>
       );
     }
-  };
-  const handelOpen = () => {
-    setIsModalOpen(true);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
   };
 
   const updateTimeline = useMutation({
@@ -144,9 +149,48 @@ const MyTimline = () => {
     };
     await updateTimeline.mutateAsync(data);
     setTwoTime(null);
-    setContent("")
+    setContent("");
   }
-
+  const contentMutation = useMutation({
+    mutationFn: async function (data: any) {
+      const result = await caxios.patch(
+        `/timeline/${selectedTimeline.data?._id}?type=content`,
+        data
+      ); // sending timeline id with updated content here
+      return result.data;
+    },
+    onSuccess: () => {
+      if (selectedTimeline.data?._id) {
+        selectedTimeline.mutate(selectedTimeline.data?._id);
+        showToast("success", `Content updated`);
+      }
+      handleCancel2();
+    },
+  });
+  function handelContentUpdate(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formdata = new FormData(e.currentTarget);
+    contentMutation.mutateAsync({
+      id: selectedSlot?._id,
+      content: String(formdata.get("content")),
+    });
+  }
+  const resetMutation = useMutation({
+    mutationFn: async () => {
+      const result = await caxios.delete(`/timeline/${selectedTimeline?.data?._id}`);
+      return result.data;
+    },
+    onSuccess: () => {
+      handleCancel2();
+      handleCancel();
+      showToast("success", "Timeline Reset");
+    },
+    onError: () => {
+      handleCancel2();
+      handleCancel();
+      showToast("error", "Reset Failed");
+    },
+  });
   return (
     <div>
       <Table
@@ -181,21 +225,38 @@ const MyTimline = () => {
           </div>
         ) : selectedTimeline.isSuccess ? (
           selectedTimeline.data.timeline.length != 0 ? (
-            <Timeline
-              mode="alternate"
-              items={selectedTimeline.data.timeline.map((x) => {
-                return {
-                  label: (
-                    <p className="font-semibold">{`${x.startTime}-${x.endTime}`}</p>
-                  ),
-                  children: (
-                    <Tooltip trigger="click" title={x._id}>
-                      {x.content}
-                    </Tooltip>
-                  ),
-                };
-              })}
-            ></Timeline>
+            <>
+              <p className=" text-red-500 italic text-right">
+                *Click each item for delete or edit
+              </p>
+              <p className="italic">
+                {moment(selectedTimeline.data.createdAt).format(
+                  "MMM Do YY, h:mm a"
+                )}
+              </p>
+              <Timeline
+                mode="alternate"
+                items={selectedTimeline.data.timeline.map((x) => {
+                  return {
+                    label: (
+                      <p
+                        className="font-semibold cursor-pointer"
+                        onClick={() => handelOpen2(x._id)}
+                      >{`${x.startTime}-${x.endTime}`}</p>
+                    ),
+                    children: (
+                      <p
+                        className="cursor-pointer"
+                        onClick={() => handelOpen2(x._id)}
+                      >
+                        {" "}
+                        {x.content}
+                      </p>
+                    ),
+                  };
+                })}
+              ></Timeline>
+            </>
           ) : (
             <p className="text-gray-600 text-center font-semibold">
               No Timeline added
@@ -219,7 +280,7 @@ const MyTimline = () => {
               setTwoTime(v);
             }}
           ></TimePicker.RangePicker>
-          <p>Description</p>
+          <p className="font-bold">Description</p>
 
           <Input.TextArea
             required
@@ -236,9 +297,53 @@ const MyTimline = () => {
               <Spin size="large"></Spin>{" "}
             </div>
           ) : (
-            <Button className="bg-orange-200 font-semibold" htmlType="submit">
-              Add
-            </Button>
+            <>
+              {resetMutation.isPending || updateTimeline.isPending ? (
+                <div className="flex justify-center">
+                  <Spin size="large"></Spin>
+                </div>
+              ) : (
+                <div className=" flex gap-2">
+                  <Button
+                    className="bg-orange-200 font-semibold flex-1"
+                    htmlType="submit"
+                  >
+                    Add
+                  </Button>
+                  <Button
+                    className="bg-red-200 font-semibold flex-1"
+                    onClick={() => {
+                      resetMutation.mutate();
+                    }}
+                  >
+                    Reset
+                  </Button>
+                </div>
+              )}
+            </>
+          )}
+        </form>
+      </Modal>
+      <Modal
+        width={500}
+        title="Time Modal"
+        destroyOnClose={true}
+        onCancel={handleCancel2}
+        footer={null}
+        open={isModalOpen2}
+      >
+        <form onSubmit={handelContentUpdate} className="flex flex-col gap-1">
+          <p className="font-semibold">{`${selectedSlot?.startTime}-${selectedSlot?.endTime}`}</p>
+          <p className="font-bold">Content</p>
+          <Input.TextArea name="content" defaultValue={selectedSlot?.content} />
+          {contentMutation.isPending ? (
+            <div className="flex justify-center">
+              <Spin size="large"></Spin>
+            </div>
+          ) : (
+            <>
+              <Button htmlType="submit">Update Content</Button>
+            </>
           )}
         </form>
       </Modal>
