5f1402ac234ac34baee8518c1a14f2ca71fa8b10
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2024-03-01 21:33:00 +0600

    complete new merchentise and 404 error

--- src/Pages/Inventory/Shop.tsx
+++ src/Pages/Inventory/Shop.tsx
@@ -1,13 +1,7 @@
-// import { Tilt } from "react-tilt";
-// import tShirt from "../assets/clock_tshirt-preview.png";
+
 import { useEffect } from "react";
 import AOS from "aos"
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-// import 'swiper/css/navigation';
 import image1 from "../../assets/tshirt/white.png";
 import image2 from "../../assets/tshirt/2 piece.png";
 import image3 from "../../assets/tshirt/black.png";
@@ -42,17 +36,6 @@ const Shop = () => {
   const handleCart = ()=>{
     navigate("/cart");
   }
-  // const defaultOptions = {
-  //   // reverse: false, // reverse the tilt direction
-  //   max: 35, // max tilt rotation (degrees)
-  //   // perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  //   scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
-  //   speed: 1000, // Speed of the enter/exit transition
-  //   // transition: true, // Set a transition on enter/exit.
-  //   // axis: null, // What axis should be disabled. Can be X or Y.
-  //   // reset: true, // If the tilt effect has to be reset on exit.
-  //   // easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
-  // };
 
   const slideData = [
     { 
@@ -84,7 +67,6 @@ const Shop = () => {
       img: image4,
     },
   ];
-
   return (
     <div className="overflow-x-hidden   hide-scrollbar relative">
       {/* hero */}
@@ -97,9 +79,6 @@ const Shop = () => {
               delay: 2500,
               disableOnInteraction: false,
             }}
-            // pagination={{
-            //   clickable: true,
-            // }}
             navigation={true}
             modules={[Autoplay, Pagination, Navigation]}
             className="mySwiper"
