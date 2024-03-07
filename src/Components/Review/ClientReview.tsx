9c5c84394bbcd3bfb94470f2568e2f66e937e422
tanzilrayhan <tanzilrayhan169@gmail.com>
2024-03-08 02:57:01 +0600

    file bug fixed

---
+++ src/Components/Review/ClientReview.tsx
@@ -0,0 +1,11 @@
+const ClientReview = () => {
+    return (
+        <div className="max-w-7xl mx-5 my-20 lg:mx-auto">
+      <h1 className="font-extrabold text-[#7c3aed] pb-10 md:py-20 text-center text-4xl md:text-5xl">
+        Clients Testimonials
+      </h1>
+    </div>
+    );
+};
+
+export default ClientReview;
